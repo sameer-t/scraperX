db7623bbe1b257ef01ba75e6ec2ab748cace4c1b
Sameer Tammana <sameer@opensilo.co>
2014-08-13 14:07:07 -0700

    Separated the scraper for getting links and for saving the links as html pages locally

---
+++ scraper_html.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+import time
+
+# Load driver.
+driver = webdriver.Chrome('/home/sameer/bin/chromedriver')
+
+# Go to LinkedIn.
+driver.get('http://www.linkedin.com/');
+
+# Login
+login_box = driver.find_element_by_name('session_key')
+password_box = driver.find_element_by_name('session_password')
+login_box.send_keys('pvora2@gmail.com')
+password_box.send_keys('Opensilo1')
+password_box.submit()
+
+links = open('./links_nest', 'r')
+users = eval(links.read())
+print len(users)
+
+errorlinks = []
+
+userno = 1
+try:
+    for ulink in users:
+        if str(type(ulink)) == "<type 'unicode'>":
+            driver.get(str(ulink))
+            time.sleep(5)
+            name = driver.find_element_by_class_name('full-name').text
+            print str(userno) + ' ' + name
+            page_html = driver.page_source.encode('utf-8')
+            user = open(str(userno)+'.html','w')
+            user.write(page_html)
+            user.close()
+            userno +=1
+        else:
+            print 'Not a link :('
+except:
+    errorlinks.append(ulink)
+    print 'Some exception'
+    
+fails = open('failed_links', 'w')
+fails.write(str(errorlinks))
+fails.close()
+
+driver.quit()
---
+++ scraper_link.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import NoSuchElementException
+
+# Load driver.
+driver = webdriver.Chrome('/home/sameer/bin/chromedriver')
+
+# Go to LinkedIn.
+driver.get('http://www.linkedin.com/');
+
+# Login
+login_box = driver.find_element_by_name('session_key')
+password_box = driver.find_element_by_name('session_password')
+login_box.send_keys('pvora2@gmail.com')
+password_box.send_keys('Opensilo1')
+password_box.submit()
+
+# Get LinkedIn profile links
+users = []
+for  in  :
+    try:
+        search_box = driver.find_element_by_name('keywords')
+        search_box.send_keys('lol')
+        search_box.submit()
+        result1 = driver.find_element_by_class_name('idx0')
+        users += result1.find_element_by_class_name('title').get_attribute('href')
+    except:
+        # record name for error
+
+links = open('links_nest', 'w')
+links.write(str(users))
+links.close()
+print 'Done saving.'
+
+driver.quit()
